1301aeb873fd9f54c89c999b26b84954f3315197
isakjohansson <johansson.isak1@gmail.com>
2020-07-29 13:49:01 +0200

    firstAction

---
+++ hello.py
@@ -0,0 +1,23 @@
+print('hello')
+
+
+import pandas as pd
+import numpy as np
+from module1 import *
+
+def brownian(drift, vol, increment=0.1):
+    return 1;
+
+
+class BS_Model():
+    def __init__(self):
+        self.test = 1
+    def simulate(self, drift, volatility):
+        """
+        simulate a gaussian distrubution multply with volatility
+        return dataframe with timestamp and values
+        """
+        pass
+        
+
+print(hello_world())
