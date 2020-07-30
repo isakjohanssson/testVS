fcbec4c4e4d37897889ce34647b0335effd73713
isakjohansson <johansson.isak1@gmail.com>
2020-07-30 11:44:49 +0200

    test2

--- hello.py
+++ hello.py
@@ -1,29 +1,35 @@
-print('hello')
-
-
 import pandas as pd
 import numpy as np
-from module1 import *
-
-def brownian(drift, vol, increment=0.1):
-    return 1;
 
 
-class BS_Model():
-    def __init__(self):
-        self.test = 1
-    def simulate(self, drift, volatility):
-        """
-        simulate a gaussian distrubution multply with volatility
-        return dataframe with timestamp and values
-        """
-        pass
-        
+class ChessBoard:
+    def __init__(self, start=(1,2)):
+        self.routes = [np.array([(x,y)]) for x in range(8) for y in range(8)]
+        self.startRoute = np.array([1,2])
+        self.current_route = np.array([1,2])
+        self.steps = [
+                        np.array([-2,-1]),
+                        np.array([-2,1]),
+                        np.array([-1,-2]),
+                        np.array([-1,2]),
+                        np.array([1,-2]),
+                        np.array([1,2]),
+                        np.array([2,-1]),
+                        np.array([2,1])
+                        ]
+        self.visited = [self.startRoute];
+        self.available_routes = [route if any([route not in self.visited for route in self.routes])]
 
-print(hello_world())
+    def number_of_possible_steps(self):
+        counter=0
+        next_possible_steps = [(self.current_route+step) for step in self.steps if any(self.current_route+step in self.available_routes)]
+        return len(next_possible_steps)
 
+my_board = ChessBoard()
 
+#print(set(my_board.routes))
+print(my_board.startRoute)
+print(my_board.current_route)
+print(my_board.current_route+my_board.current_route)
 
-print('How will this part look in the github view?')
-
-print('nuddaaaaa')
+print(my_board.number_of_possible_steps())
