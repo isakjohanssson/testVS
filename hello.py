d7e7ed5618e86e61a5a8f24585792904a161cd3d
isakjohansson <johansson.isak1@gmail.com>
2021-01-01 19:18:02 +0100

    removed some spaces

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
 class Route:
-
-
     def __init__(self, position, possible_next_positons=None):
         self.position = position
         self.possible_next_positions = possible_next_positons
@@ -15,8 +13,6 @@ class Route:
 
 
 class ChessBoard:
-
-
     def __init__(self, start=(0,0)):
         self.routes = [(x,y) for x in range(8) for y in range(8)]
         self.steps = [
@@ -30,8 +26,20 @@ class ChessBoard:
                         [2,1]
                         ]
         self.visited = [Route(start)]
-        #korta ned nästa rad avsevärt, gärna kombinera med raden ovan.
-        self.visited[-1].set_next_positions(self.get_ordered_next_positions(self.get_next_positions(start), self.get_number_of_possible_next_next_positions(self.get_next_positions(start))))
+        self.is_filled = False
+
+        '''
+        Add ordered next positions from 
+        next positions and number of next positions from the next positions
+        '''
+        self.visited[-1].set_next_positions(
+            self.get_ordered_next_positions(        
+                self.get_next_positions(start),
+                self.get_number_of_possible_next_positions(
+                    self.get_next_positions(start)
+                                                            )
+                                            ))
+
     
     def get_available_positions(self):
         return [route for route in self.routes if route not in [route.position for route in self.visited]]
@@ -39,54 +47,55 @@ class ChessBoard:
     def get_next_positions(self, from_position=(0,0)):
         return [next_position for step in self.steps if (next_position:= (from_position[0]+step[0], from_position[1]+step[1])) in self.get_available_positions()]
     
-    def get_number_of_possible_next_next_positions(self, next_positons):
+    def get_number_of_possible_next_positions(self, next_positons):
         return [len(self.get_next_positions(steps_from_next_position)) for steps_from_next_position in next_positons]
 
-    def get_ordered_next_positions(self, next_route_positions, number_of_possible_next_next_positions):
+    def get_ordered_next_positions(self, next_route_positions, number_of_possible_next_positions):
             #slå ihop med get_next_positions och strukturera om. Vore snyggare
-            dummy_list = [[elem[0] for elem in next_route_positions],[elem[1] for elem in next_route_positions], number_of_possible_next_next_positions]
-            dummy_list = [(next_route_positions[i][0], next_route_positions[i][1], number_of_possible_next_next_positions[i]) for i in range(len(number_of_possible_next_next_positions))]
+            dummy_list = [[elem[0] for elem in next_route_positions],[elem[1] for elem in next_route_positions], number_of_possible_next_positions]
+            dummy_list = [(next_route_positions[i][0], next_route_positions[i][1], number_of_possible_next_positions[i]) for i in range(len(number_of_possible_next_positions))]
             dummy_list.sort(key = lambda x:x[2])
-            #dummy_list = [(elem[0], elem[1]) for elem in dummy_list]
             return [(elem[0], elem[1]) for elem in dummy_list]
 
     def do_next_step(self):
-        #korta ned och flytta till fill_board
         if (route_position:= route[0] if (route:=self.visited[-1].possible_next_positions) else False):
             self.visited.append(Route(route_position))
 
             next_route_positions = self.get_next_positions(route_position)
-            number_of_next_next_positions = self.get_number_of_possible_next_next_positions(next_route_positions)
-            ordered_next_positions = self.get_ordered_next_positions(next_route_positions, number_of_next_next_positions)
+            number_of_next_positions = self.get_number_of_possible_next_positions(next_route_positions)
+            ordered_next_positions = self.get_ordered_next_positions(next_route_positions, number_of_next_positions)
             
             self.visited[-1].set_next_positions(ordered_next_positions)
-            #Do further debugs here
-            print('--------')
-            print(self.visited[-1].position)
-            print(self.visited[-1].possible_next_positions)
-            print('Dessa rutor har besökts ', [elem.position for elem in self.visited])
-            print('Dessa rutor är tillgängliga', self.get_available_positions())
-            print('--------')
-            #take a step back
-        #borde inte ingå i denna funktion
+            
         elif len(self.visited)<64:
             self.take_step_back()
         return
 
     def take_step_back(self):
-        print('Not yet done')
+        # take a step back and don't take the same step again
         self.visited.pop()
         self.visited[-1].pop()
         return
 
     def fill_board(self):
-        for i in range(1000):
+        while not self.is_filled:
             self.do_next_step()
+            if len(self.visited) == 64:
+                self.is_filled = True
         return
 
 
 
-my_board = ChessBoard(start=(1,7))
+my_board = ChessBoard(start=(1,2))
 
 my_board.fill_board()
 print(len(my_board.visited))
+
+all_positions = [[i,j] for i in range(8) for j in range(8)]
+print(all_positions)
+
+#for position in all_positions:
+   # my_board = ChessBoard(start=(position[0],position[1]))
+    #my_board.fill_board()
+
+
